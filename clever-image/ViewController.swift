7599a69fefb2327987703d8e34e6f1d8a7e1adcc
Mehmet Koca <mehmetkoca@outlook.com>
2017-08-23 01:24:53 +0300

    completed pickerView

---
+++ clever-image/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  clever-image
+//
+//  Created by Mehmet Koca on 22/08/2017.
+//  Copyright © 2017 on3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func chooseBtnTapped(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
+
